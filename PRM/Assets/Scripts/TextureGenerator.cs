1e7fa6f9491e13096b72b2e3832e05478f47ed79
asraful <asraful009@gmail.com>
2021-09-04 18:04:43 +0600

    map

--- PRM/Assets/Scripts/TextureGenerator.cs
+++ PRM/Assets/Scripts/TextureGenerator.cs
@@ -7,6 +7,8 @@ public static class TextureGenerator
     public static Texture2D TextureFromColorMap(Color[] colorMap, int mapWidth, int mapHeight)
     {
         Texture2D texture = new Texture2D(mapWidth, mapHeight);
+        texture.filterMode = FilterMode.Point;
+        texture.wrapMode = TextureWrapMode.Clamp;
         texture.SetPixels(colorMap);
         texture.Apply();
         return texture;
